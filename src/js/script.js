5fcc341b1e7a1c74be5d24eb26b9ec1c34b1a6c1
“Deddoctor30”~ <alinsbomb@yandex.ru>
2022-02-13 15:59:06 +0300

    13.02.2022

---
+++ src/js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+   // Меню бургер
+   const inner = document.querySelector('.header__inner'),
+         line = document.querySelector('.header__line'),
+         menu = document.querySelector('.header__menu'),
+         bar = document.querySelector('.header__right-bar'),
+         burgerTrigger = document.querySelector('.burger');
+
+
+   burgerTrigger.addEventListener('click', () => {
+      inner.classList.toggle('menu-burger');
+      line.classList.toggle('line-wide');
+      menu.classList.toggle('menu-burger');
+      bar.classList.toggle('menu-burger');
+      burgerTrigger.classList.toggle('hide');
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
