3861658c0f409601d7bf8fb4a2f37fdfb5abb04c
“Deddoctor30”~ <alinsbomb@yandex.ru>
2022-02-15 21:46:31 +0300

    +slider

--- src/js/script.js
+++ src/js/script.js
@@ -46,89 +46,288 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-   // Слайдер - каруселька
-   const items = document.querySelectorAll('.blog__item'),
-         wrapper = document.querySelector('.blog__wrapper'),
-         slideField = document.querySelector('.blog__items'),
-         width = window.getComputedStyle(wrapper).width,
-         arrowNext = document.querySelector('.blog__arrow-next'),
-         arrowPrev = document.querySelector('.blog__arrow-prew');
+   // // Слайдер - каруселька
+   // const items = document.querySelectorAll('.blog__item'),
+   //       wrapper = document.querySelector('.blog__wrapper'),
+   //       slideField = document.querySelector('.blog__items'),
+   //       width = window.getComputedStyle(wrapper).width,
+   //       arrowNext = document.querySelector('.blog__arrow-next'),
+   //       arrowPrev = document.querySelector('.blog__arrow-prew');
 
-   let offset = 0;
+   // let offset = 0;
+
+   // slideField.style.width = 100 * items.length + '%';
+
+   // items.forEach(item => {
+   //    item.style.width = width;
+   // });
+
+   // arrowNext.addEventListener('click', () => {
+   //    if (offset >= +width.replace(/\D/g, '') * (items.length - 1)) {
+   //       offset = 0;
+   //    } else {
+   //       offset += +width.replace(/\D/g, '');
+   //    }
+
+   //    slideField.style.transform = `translateX(-${offset}px)`
+   // });
+
+   // arrowPrev.addEventListener('click', () => {
+   //    if (offset == 0) {
+   //       offset = +width.replace(/\D/g, '') * (items.length - 1);
+   //    } else {
+   //       offset -= +width.replace(/\D/g, '');
+   //    }
+
+   //    slideField.style.transform = `translateX(-${offset}px)`;
+   // })
+
+
+
+
+
+
+   // // Тач слайдер
+   // let start = 0;
+   // let move = 0;
+   // let tempOff = 0;
+   // let endTotal = 0;
+   // let n = 1;
+
+
+   // // Событие нажать
+   // wrapper.addEventListener('mousedown', swipeMove);
+
+   // function swipeMove (event) {
+   //    start = event.clientX;
+   //    event.preventDefault();
+
+   //    // Событие move
+   //    wrapper.addEventListener('mousemove', moveFunc);
+
+   //    function moveFunc (event) {
+   //       move = start - event.clientX;
+   //       tempOff = move + endTotal;
+
+
+   //       if ((move >= +width.replace(/\D/g, '') / 2.5)) {
+   //          nextSlide();
+   //       }
+
+   //       if (((Math.abs(move) >= +width.replace(/\D/g, '') / 2.5)) && (move < 0)) {
+   //          prevSlide();
+   //       }
+
+   //       if (tempOff > (+width.replace(/\D/g, '') * (items.length - 1)) + 120) {
+   //          tempOff = 0;
+   //       }
+
+   //       if (tempOff < -120) {
+   //          tempOff = +width.replace(/\D/g, '') * (items.length - 1);
+   //       }
+
+   //       if (move > 0) {
+   //          slideField.style.transform = `translateX(-${tempOff}px)`;
+   //       } else {
+   //          slideField.style.transform = `translateX(${tempOff * -1}px)`;
+   //       }
+   //    }
+
+   //    // Событие отжать
+   //    window.addEventListener('mouseup', () => {
+   //       endTotal = tempOff;
+   //       wrapper.removeEventListener('mousemove', moveFunc);
+   //    });
+
+   //    function nextSlide () {
+   //       tempOff = n * (+width.replace(/\D/g, ''));
+   //       wrapper.removeEventListener('mousemove', moveFunc);
+   //       if (n === items.length) {
+   //          n = 1;
+   //       } else {
+   //          n++;
+   //       }
+   //    }  
+
+   //    function prevSlide () {
+   //       if (n === 1) {
+   //          n = items.length;
+   //       } else {
+   //          n--;
+   //       }
+
+   //       tempOff = (n - 1) * (+width.replace(/\D/g, ''));
+   //       wrapper.removeEventListener('mousemove', moveFunc);
+   //    }
+   // }
+
+
+
+   function nextSlide (value) {
+      slideField.style.transform = `translateX(-${value}px)`
+      console.log(value);
+   }
+
+
+   // Переменные
+      // Слайдер - каруселька
+         const items = document.querySelectorAll('.blog__item'),
+               wrapper = document.querySelector('.blog__wrapper'),
+               slideField = document.querySelector('.blog__items'),
+               width = window.getComputedStyle(wrapper).width,
+               arrowNext = document.querySelector('.blog__arrow-next'),
+               arrowPrev = document.querySelector('.blog__arrow-prew');
+
+
+      // Тач слайдер
+         let start = 0;
+         let move = 0;
+         let offset = 0;
+         let endTotal = 0;
+         let n = 1;
+         let tempOff = 0;
+
+         
 
    slideField.style.width = 100 * items.length + '%';
 
    items.forEach(item => {
       item.style.width = width;
    });
+   
 
+   // Слайд вперед по клику стрелки
    arrowNext.addEventListener('click', () => {
-      if (offset >= +width.replace(/\D/g, '') * (items.length - 1)) {
+      offset += +width.replace(/\D/g, '');
+      if (offset >= +width.replace(/\D/g, '') * (items.length)) {
          offset = 0;
-      } else {
-         offset += +width.replace(/\D/g, '');
       }
+      // Попробовать объединить функции      
+      // nextSlideEV ();
 
-      slideField.style.transform = `translateX(-${offset}px)`
+
+      if (n === items.length) {
+         n = 1;
+      } else {
+         n++;
+      }
+      nextSlide(offset);
+      console.log(`N = ${n}`);
    });
 
+
+
+   // Слайд назад по клику стрелки
    arrowPrev.addEventListener('click', () => {
-      if (offset == 0) {
+      offset -= +width.replace(/\D/g, '');
+      if (offset < 0) {
          offset = +width.replace(/\D/g, '') * (items.length - 1);
-      } else {
-         offset -= +width.replace(/\D/g, '');
       }
 
-      slideField.style.transform = `translateX(-${offset}px)`;
-   })
+      if (n === 1) {
+         n = items.length;
+      } else {
+         n--;
+      }
+
+      nextSlide(offset);
+      console.log(`N = ${n}`);
+   });
 
 
-   // Курсор
-   let start = 0;
-   let move = 0;
-   let end = 0;
-   let tempOff = 0;
 
+//_____________________________
+
+
+
+
+   // Событие нажать
    wrapper.addEventListener('mousedown', swipeMove);
 
+   // Функция нажать
    function swipeMove (event) {
+      endTotal = offset;
+
       start = event.clientX;
-      console.log(`Кликнул ${start}`);
       event.preventDefault();
 
+      // Событие move
       wrapper.addEventListener('mousemove', moveFunc);
 
+
+      // Функция move
       function moveFunc (event) {
-         move = event.clientX;
-         offset = start - move;
-         console.log(offset);
-         slideField.style.transform = `translateX(-${offset}px)`;
+         move = start - event.clientX;
+         offset = move + endTotal;
+
+         // Проверка выравнить слайд по центру при свайпе вправо  [коэффициент 2.5 - доля от ширины слайдера, при которой произойдет выравнивание]
+         if ((move >= +width.replace(/\D/g, '') / 2.5)) {
+            nextSlideE();
+         }
+
+         // Проверка выравнить слайд по центру при свайпе влево   [коэффициент 2.5 - доля от ширины слайдера, при которой произойдет выравнивание]
+         if (((Math.abs(move) >= +width.replace(/\D/g, '') / 2.5)) && (move < 0)) {
+            prevSlideE();
+         }
+
+         // Перемотка в начало
+         if (offset > (+width.replace(/\D/g, '') * (items.length - 1)) + 120) {
+            offset = 0;
+         }
+
+         // Перемотка в конец
+         if (offset < -120) {
+            offset = +width.replace(/\D/g, '') * (items.length - 1);
+         }
+         console.log(`Перед value ${offset}`);
+         nextSlide(Math.abs(offset));
       }
 
-      window.addEventListener('mouseup', (event) => {
-         end = event.clientX;
+      // Событие отжать
+      window.addEventListener('mouseup', () => {
+         // Запоминаем значение мува при отпускании мыши, чтобы при следующем клике значения ++
+         endTotal = offset;
+
+         // Останавливаем функцию слежения при клике
          wrapper.removeEventListener('mousemove', moveFunc);
-         console.log(`Отпустил ${end}`);
-         // if (offset >= (+width.replace(/\D/g, '') / 2)) {
-         //    offset += +width.replace(/\D/g, '');
-         // }
       });
+
+
+      // Выравнить слайд по центру при свайпе вправо
+      function nextSlideE () {
+         // Останавливаем функцию слежения при выравнивании слайда по центру
+         wrapper.removeEventListener('mousemove', moveFunc);
+         nextSlideEV ();
+      }  
+
+      // Выравнить слайд по центру при свайпе влево
+      function prevSlideE () {
+         // Останавливаем функцию слежения при выравнивании слайда по центру
+         wrapper.removeEventListener('mousemove', moveFunc);
+         prevSlideEV ();
+      }
    }
 
 
+   // Выравнить слайд по центру при свайпе вправо
+   function nextSlideEV () {
+      offset = n * (+width.replace(/\D/g, ''));
+      if (n === items.length) {
+         n = 1;
+      } else {
+         n++;
+      }
+   }  
 
-   // console.log(move);
-
-   // wrapper.addEventListener('mousemove', (event) => {
-   //    move = event.clientX;
-   // });
-
-   // wrapper.addEventListener('mouseup', (event) => {
-   //    end = event.clientX;
-   // });
-
-   
-
+   // Выравнить слайд по центру при свайпе влево
+   function prevSlideEV () {
+      if (n === 1) {
+         n = items.length;
+      } else {
+         n--;
+      }
+      offset = (n - 1) * (+width.replace(/\D/g, ''));
+   }
 
 
 
