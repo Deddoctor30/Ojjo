33d07a640ab5c170eb4cd806e1891af06a84d615
“Deddoctor30”~ <alinsbomb@yandex.ru>
2022-05-17 15:13:57 +0300

    17.05.2022

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -42,7 +42,6 @@ function cardSlider() {
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
-      // fade: false,
       dots: false,
       asNavFor: '.product__bottop-images',
       responsive: [{
@@ -53,23 +52,7 @@ function cardSlider() {
           infinite: true,
           dots: true
         }
-      } // {
-      //    breakpoint: 716,
-      //    settings: {
-      //    slidesToShow: 1,
-      //    slidesToScroll: 1,
-      //    dots: true
-      //    }
-      // },
-      // {
-      //    breakpoint: 544,
-      //    settings: {
-      //    slidesToShow: 2,
-      //    slidesToScroll: 2,
-      //    dots: true
-      //    }
-      // }
-      ]
+      }]
     });
     $('.product__bottop-images').slick({
       slidesToShow: 2,
@@ -83,41 +66,7 @@ function cardSlider() {
         breakpoint: 1024,
         settings: "unslick"
       }]
-    }); // $('.product__main-image').slick({
-    //    dots: false,
-    //    infinite: true,
-    //    speed: 300,
-    //    slidesToShow: 6,
-    //    adaptiveHeight: true,
-    //    arrows: false,
-    //    responsive: [
-    // {
-    //    breakpoint: 1024,
-    //    settings: {
-    //    slidesToShow: 4,
-    //    slidesToScroll: 2,
-    //    infinite: true,
-    //    dots: true
-    //    }
-    // },
-    // {
-    //    breakpoint: 716,
-    //    settings: {
-    //    slidesToShow: 3,
-    //    slidesToScroll: 2,
-    //    dots: true
-    //    }
-    // },
-    // {
-    //    breakpoint: 544,
-    //    settings: {
-    //    slidesToShow: 2,
-    //    slidesToScroll: 2,
-    //    dots: true
-    //    }
-    // }
-    // ]
-    // });
+    });
   }
 }
 
